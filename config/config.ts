7c40144bfdc69c362b6cb2e13a0a86cf7a26e79f
Saad Ahmed (Desktop) <saad_vamerce@gmail.com>
2020-10-01 22:56:02 +0500

    Port update

--- config/config.ts
+++ config/config.ts
@@ -3,13 +3,13 @@ let conf;
 switch(process.env.NODE_ENV) {
     case 'production':
         conf = {
-            PORT: 9800,
+            PORT: 80,
             DBUrl: 'mongodb://localhost:27017/',
             DBName: 'seedcodeDB'
         }
     default:
         conf = {
-            PORT: 9800,
+            PORT: 80,
             DBUrl: 'mongodb://localhost:27017/',
             DBName: 'seedcodeDB'
         }
